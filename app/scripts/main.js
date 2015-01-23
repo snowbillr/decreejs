cf526918b2d7c1cbf8a7a3e476dc76c37518bacd
Bill Reed <william.reed@ge.com>
2015-01-23 11:36:12 -0800

    updated main.js to be the same as what the tutorial is saying

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,17 @@
-decree('a').then('s').then('d').then('f').perform(function() {
-    console.log('"asdf" was pressed');
+decree('a').perform(function() {
+    console.log('"a" was pressed');
 });
 
-decree('a').then('a').then('a').then('a').perform(function() {
-    console.log('"aaaa" was pressed');
+decree('q').then('w').then('e').then('r').then('t').then('y').perform(function() {
+    console.log('"qwerty" was pressed');
+});
+
+decree('w').then('o').perform(function() {
+    console.log('I will be called because the "wo" key sequence is shorter than "woo".');
+});
+
+decree('w').then('o').then('o').perform(function() {
+    console.log('I will never be called because the "wo" key sequence is shorter than "woo".');
 });
 
 decree('up').then('up').then('down').then('down').then('left').then('right').then('left').then('right').then('b').then('a').then('enter').perform(function() {
